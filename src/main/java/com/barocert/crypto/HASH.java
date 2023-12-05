98f468784f084e3fc4f2eb07d02dbddf7f167235
정우석 <wsj@linkhubcorp.com>
2023-12-05 21:08:57 +0900

    sha256 함수 수정

--- src/main/java/com/barocert/crypto/HASH.java
+++ src/main/java/com/barocert/crypto/HASH.java
@@ -1,25 +1,23 @@
 package com.barocert.crypto;
 
 import java.security.MessageDigest;
-import java.util.Base64;
-
 import com.barocert.BarocertException;
 
 public class HASH {
 
-    public String sha256(String target) throws BarocertException {
-	
-        byte[] sha256EncodedData; 
+	public String sha256(String target) throws BarocertException {
+
+		String sha256EncodedData;
 		try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update(target.getBytes());
-            
-			sha256EncodedData = Base64.getUrlEncoder().encode(md.digest());
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(target.getBytes());
+
+			sha256EncodedData = Base64.encode(md.digest()).replace("+", "-").replace("/", "_").replace("=", "");
 		} catch (Exception e) {
 			throw new BarocertException(-99999999, "해시 생성에 실패하였습니다.");
 		}
-		
-        return new String(sha256EncodedData);
+
+		return sha256EncodedData;
 	}
 
 }
